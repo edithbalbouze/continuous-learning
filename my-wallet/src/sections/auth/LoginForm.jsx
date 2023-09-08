ac7941b676e9d8d01dfb87058fecae6d0b1a65f2
Edith Balbouze <edithbalbouze@Ediths-MacBook-Air-2.local>
2023-09-08 15:52:41 -0400

    conexion con flexio y embellecimiento de la aplicacion

--- my-wallet/src/sections/auth/LoginForm.jsx
+++ my-wallet/src/sections/auth/LoginForm.jsx
@@ -1,15 +1,37 @@
+import {useState} from "react";
+import axios from "axios";
+
 export const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeEmail = ({target}) => setEmail(target.value);
+    const onChangePassword = ({target}) => setPassword(target.value);
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        const { data } = await axios.post('https://implementaciones.pensanomica.com/clientes/flexio/API_Flexio/login_master', {
+            email,
+            password
+        });
+        const apiToken = data.data.api_token;
+
+        localStorage.setItem('token', apiToken);
+    };
+
+
     return (
         <div className="login">
             <div className="form-container">
                 <img className="logo"  alt="logo" src='src/assets/logo-svg.svg'/>
-                <form className="form">
+                <form className="form" onSubmit={handleLogin}>
 
                     <label htmlFor={"email"} className="label"  >Correo Electrónico</label>
-                    <input className="input" id={"email"} placeholder="ejemplo@medine.tech"/>
+                    <input className="input" id={"email"} placeholder="ejemplo@medine.tech" value={email} onChange={onChangeEmail} />
 
                     <label htmlFor={"password"} className="label"  >Contraseña</label>
-                    <input  className="input" id={"password"} placeholder="************"/>
+                    <input  className="input" type="password" id={"password"} placeholder="************" value={password} onChange={onChangePassword} />
 
                     <button className="primary-button">Iniciar sesión</button>
 
